2068af091e7e8eed2224da54f861fdbd55147d77
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-08-02 14:49:34 -0300

    Requisito 7 completo!

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Table extends Component {
+class Table extends Component {
+  formatValue = (n) => n.toFixed(2);
+
+  handleValue = (n1, n2 = 1) => {
+    if (typeof n1 !== 'number') {
+      parseFloat(n1);
+    }
+    if (typeof n2 !== 'number') {
+      parseFloat(n2);
+    }
+    return this.formatValue(n1 * n2);
+  };
+
   render() {
+    const { expenses } = this.props;
+
     return (
-      <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+      <table id="tabela">
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+        <tbody>
+          { expenses.length > 0 && expenses
+            .map(({ id, description, tag, method, value, currency, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{this.handleValue(value)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{this.handleValue(exchangeRates[currency].ask)}</td>
+                <td>{this.handleValue(value, exchangeRates[currency].ask)}</td>
+                <td>BRL</td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     );
   }
 }
+
+Table.propTypes = ({
+  expenses: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+}.isRequired);
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
